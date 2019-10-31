f21439c1c3af78f9646ef22515d91b7778361322
minawoww <mina.medhat100@gmail.com>
2019-10-31 12:23:57 +0200

    Adding Documentations

--- ESP8266_Driver.c
+++ ESP8266_Driver.c
@@ -9,7 +9,6 @@ static uint8_t G_ReceivedData = 0;
 static EnumRx_Status_t Rx_Status = Rx_NotComplete;
 
 
-
 static void UART_RxString(uint8_t Data)
 {
 	static uint8_t Read_Flag = 0;
@@ -30,15 +29,18 @@ static void UART_RxString(uint8_t Data)
 
 void ESP_Init(void){
 	
-	// UART init
+	// UART init WITH BAUD RATE AS WIFI CONFIGURED AS 9600 BSP
 	UART_Init(9600,UART_RxString);
-	
+	// SET UP THE MODE ON 2 AS AN ACCESS POINT
 	UART_Write_String("AT+CWMODE=2\r\n");
 	_delay_ms(1000);
+	// CONFIGURE THE ACCESS POINT NAME AND PASSWORD
 	UART_Write_String("AT+CWSAP_DEF=\"dabQADER\",\"123456789\",5,3\r\n");
 	_delay_ms(2000);
+	// ENABLE MULTIPLE CONNECTIONS
 	UART_Write_String("AT+CIPMUX=1\r\n");
 	_delay_ms(1000);
+	// CREATE SERVER AT PORT NUMBER
 	UART_Write_String("AT+CIPSERVER=1,2222\r\n");
 	_delay_ms(1000);
 	
